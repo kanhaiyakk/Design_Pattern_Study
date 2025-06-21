5ef5b1538d3ae7d6916d617ab112b3fd42647ec3
kanhaiyakk <kanhaiya.kk20598@gmail.com>
2025-06-21 14:08:44 +0530

    second commit

---
+++ src/Singleton_Pattern/Browser.java
@@ -0,0 +1,15 @@
+package Singleton_Pattern;
+
+public class Browser {
+    private static Browser browser;
+    private Browser() {};
+    public static Browser getInstance(){
+        if (browser==null){
+            browser=new Browser();
+        }
+        return browser;
+    }
+    public void displayMessage(){
+        System.out.println("Browser info");
+    }
+}
---
+++ src/Singleton_Pattern/TestBrowser.java
@@ -0,0 +1,7 @@
+package Singleton_Pattern;
+
+public class TestBrowser {
+    public static void main(String[] args) {
+        Browser.getInstance().displayMessage();
+    }
+}
